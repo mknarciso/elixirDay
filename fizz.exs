ef830beb1b6ca63fd2a3bc00e3184fbcada076f7
Murilo K Narciso <mknarciso@hotmail.com>
2016-10-08 14:42:03 -0300

    Arquivos necessários

--- fizz.exs
+++ fizz.exs
@@ -4,8 +4,8 @@ defmodule FizzBuzz do
   def host, do: {192, 168, 16, 107} #'localhost'
   def port, do: 8080
   def filename, do: __ENV__.file
-  def team, do: "teste"
-  def password, do: "perdi"
+  def team, do: "Pano Branco Pato fu"
+  def password, do: "244466666"
   def challenge, do: "FizzBuzz"
 
   def fizzbuzz(i), do: fizzbuzz(rem(i,3), rem(i,5), i)
@@ -13,16 +13,17 @@ defmodule FizzBuzz do
   def fizzbuzz(0,_,_), do: "Fizz"
   def fizzbuzz(_,0,_), do: "Buzz"
   def fizzbuzz(_,_,n), do: n
-
  
   def solve(input) do
     [start,finish] = input
        |> String.split
        |> Enum.map(&String.to_integer/1)
 
-    start..finish |> Enum.reduce([], fn x, acc ->  [ fizzbuzz(x) |acc] end)
+    result = start..finish |> Enum.reduce([], fn x, acc ->  [ fizzbuzz(x) |acc] end)
        |> Enum.reverse
        |> Enum.join("\n")
+    result <> "\n"
+
   end
 
 end
